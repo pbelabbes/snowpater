85b8045e7db081874486bf0c1d0e1affb085d204
Pierre Belabbes <pbelabbes@gmail.com>
2020-03-21 18:59:26 +0100

    design a card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
+import { CardComponent } from './card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/constants.ts
@@ -0,0 +1,8 @@
+export const CONSTANT = {
+    color : {
+        blue:"#3498db",
+        red:"#e74c3c",
+        neutral:"#BDC581",
+        assassin:"#2C3A47"
+    }
+}
