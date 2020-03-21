97ca8946c9565c40da5500c3f36079bde6a37cc7
Pierre Belabbes <pbelabbes@gmail.com>
2020-03-21 19:44:27 +0100

    grid ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CardComponent } from './card/card.component';
+import { CardComponent } from './cards-grid/card/card.component';
 import {MatCardModule} from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardsGridComponent } from './cards-grid/cards-grid.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    CardsGridComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cards-grid/cards-grid.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { DICO } from 'src/assets/words-list';
+
+@Component({
+  selector: 'app-cards-grid',
+  templateUrl: './cards-grid.component.html',
+  styleUrls: ['./cards-grid.component.scss']
+})
+export class CardsGridComponent implements OnInit {
+  
+  words:string[]=[];
+  GRIDSIZE=25;
+  
+  constructor() { }
+  
+  ngOnInit() {
+    
+    this.initWords();
+  }
+  
+  private initWords(){
+    const wordsStack = DICO;
+    for(let i = 0; i < this.GRIDSIZE; i++){
+      const index = Math.floor(Math.random() * wordsStack.length);
+      const word = wordsStack.splice(index,1)[0];
+      this.words.push(word);
+    }
+    console.log(this.words);
+  }
+}
